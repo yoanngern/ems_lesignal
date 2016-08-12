b460173a5b9ef3adae25e958fc61db7c3008c225
Yoann Gern <yoann@yoanngern.ch>
2016-08-12 09:29:42 +0200

    add icons

--- wp-content/themes/ems_lesignal/header.php
+++ wp-content/themes/ems_lesignal/header.php
@@ -6,13 +6,26 @@
 	<meta name="viewport"
 	      content="initial-scale=1, width=device-width, minimum-scale=1, user-scalable=no, maximum-scale=1, width=device-width, minimal-ui">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="apple-touch-icon" href=""/>
-	<link rel="shortcut icon" href="" type="image/x-icon">
-	<link rel="icon" type="image/png" href=""/>
+
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();?>/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon_57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon_72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon_76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon_114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon_120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon_144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri();?>/apple-touch-icon_152.png" />
+
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB-KefdnvHYruJR7sGAOolnbNMMdA2z_uA"></script>
+
+	<?php if ( $post->post_name == "contact" ): ?>
+
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB-KefdnvHYruJR7sGAOolnbNMMdA2z_uA"></script>
+
+	<?php endif; ?>
 	<?php wp_head(); ?>
 
 
